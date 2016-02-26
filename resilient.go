dd71056da9d88b027810ae97d1e0c562ea1807d5
Tomas Aparicio <tomas@aparicio.me>
2016-02-26 12:47:40 +0000

    fix(style): pass golint

--- resilient.go
+++ resilient.go
@@ -7,20 +7,23 @@ import (
 	"net/http"
 )
 
+// New creates a new gentleman based HTTP client.
 func New() *gentleman.Client {
 	return gentleman.New()
 }
 
+// NewRetryClient creates a new HTTP client with constant
+// retry back off strategy in case of failure.
 func NewRetryClient() *gentleman.Client {
 	cli := New()
 	cli.Use(retry.New(nil))
 	return cli
 }
 
+// NewExponentialRetryClient creates a new HTTP client with
+// exponential retry strategy in case of failure.
 func NewExponentialRetryClient() *gentleman.Client {
 	cli := New()
 	cli.Use(retry.New(retry.New(retrier.New(retrier.ExponentialBackoff(3, 100*time.Millisecond), nil))))
 	return cli
 }
-
-// TODO: add consul client
--- version.go
+++ version.go
@@ -1,3 +1,4 @@
 package resilient
 
+// Version exposes the current semantic version of the package
 const Version = "0.1.0"
