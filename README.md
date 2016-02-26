81a2ce6b4657df0a289fd79735cc6f1584fda0fc
Tomas Aparicio <tomas@aparicio.me>
2016-02-26 12:37:37 +0000

    refactor(client): use gentleman

--- README.md
+++ README.md
@@ -1,47 +1,23 @@
-# resilient.go [![Build Status](https://travis-ci.org/resilient-http/resilient.go.png)](https://travis-ci.org/resilient-http/resilient.go) [![GitHub release](http://img.shields.io/github/tag/resilient-http/resilient.go.svg?style=flat-square)](https://github.com/resilient-http/resilient.go/releases) [![GoDoc](https://godoc.org/github.com/resilient-http/resilient.go?status.svg)](https://godoc.org/github.com/resilient-http/resilient.go) [![Coverage Status](https://coveralls.io/repos/resilient-http/resilient.go/badge.svg?branch=master)](https://coveralls.io/r/resilient-http/resilient.go?branch=master)
+# resilient.go [![Build Status](https://travis-ci.org/resilient-http/resilient.go.png)](https://travis-ci.org/resilient-http/resilient.go) [![GitHub release](http://img.shields.io/github/tag/resilient-http/resilient.go.svg?style=flat-square)](https://github.com/resilient-http/resilient.go/releases) [![GoDoc](https://godoc.org/github.com/resilient-http/resilient.go?status.svg)](https://godoc.org/github.com/resilient-http/resilient.go)
 
-Middleware-oriented, plugable and fault tolerant HTTP client written in [Go](http://golang.org) designed 
-for distributed and [reactive](http://www.reactivemanifesto.org/) systems
+resilient HTTP client for [Go](http://golang.org) built on top of [gentleman](https://github.com/h2non/gentleman) HTTP toolkit.
 
-This implementation was originally based on [resilient.js](https://github.com/resilient-http/resilient.js), 
-but focused on better design, extensibility and simplicity.
+Designed for distributed systems with fault tolerance capabilities. 
 
-Requires Go +1.3
+Still beta.
 
-**Work in progress**
+## Features
 
-## Superpowers
-
-- Full featured HTTP client build on top of `net/http`
-- No third-party dependencies (only native packages are used)
-- Smart fail over (via multiple strategies)
-- Dynamic server discovery (via middleware)
-- Retries cycles 
-- Client side based balancing and load distribution (via)
-
-**This is a work in progress**
+- Simple, expressive, fuent API.
+- Plugin driven architecture.
+- Built on top of `net/http`.
+- 
 
 ## Installation
 
+```bash
+go get -u gopkg.in/resilient-http/resilient.go.v0
 ```
-go get gopkg.in/resilient-http/resilient.go.v0
-```
-
-## Addons
-
-### Strategies
-
-- Failover
-
-### Middlewares
-
-- Server discovery
-
-## Rationale
-
-Distributed and reactive system architectures are growing during last years, but they are mostly hard to support since most of the complexibility is delegated in the consumer side.
-
-Resilient aims to simplify this to deal elegantly with distributed systems
 
 ## License 
 
--- resilient.go
+++ resilient.go
@@ -1,108 +1,26 @@
 package resilient
 
 import (
-	"gopkg.in/resilient-http/resilient.go.v0/client"
-	"gopkg.in/resilient-http/resilient.go.v0/middlewares"
-	"gopkg.in/resilient-http/resilient.go.v0/strategies"
-	"io"
+	"gopkg.in/eapache/go-resiliency.v1/retrier"
+	"gopkg.in/h2non/gentleman-retry.v0"
+	"gopkg.in/h2non/gentleman.v0"
 	"net/http"
-	"sync"
 )
 
-type Resilient struct {
-	sync.Mutex
-	Servers     []string
-	strategies  []strategies.Strategy
-	middlewares *middlewares.Middlewares
+func New() *gentleman.Client {
+	return gentleman.New()
 }
 
-func New() *Resilient {
-	return &Resilient{
-		middlewares: middlewares.New(),
-	}
+func NewRetryClient() *gentleman.Client {
+	cli := New()
+	cli.Use(retry.New(nil))
+	return cli
 }
 
-func (r *Resilient) UseServers(servers []string) {
-	r.Lock()
-	defer r.Unlock()
-	r.Servers = servers
+func NewExponentialRetryClient() *gentleman.Client {
+	cli := New()
+	cli.Use(retry.New(retry.New(retrier.New(retrier.ExponentialBackoff(3, 100*time.Millisecond), nil))))
+	return cli
 }
 
-func (r *Resilient) Use(m middlewares.Handler) error {
-	r.Lock()
-	defer r.Unlock()
-
-	handler, err := m(r)
-	if err == nil {
-		r.middlewares.Add(handler)
-	}
-
-	return err
-}
-
-func (r *Resilient) UseStrategy(s strategies.Strategy) {
-	r.Lock()
-	defer r.Unlock()
-	r.strategies = append(r.strategies, s)
-}
-
-func (r *Resilient) Get(url string) (*http.Response, error) {
-	o := &client.Options{
-		Method: "GET",
-		URL:    url,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Post(url string, body io.Reader) (*http.Response, error) {
-	o := &client.Options{
-		Method: "POST",
-		URL:    url,
-		Body:   body,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Put(url string, body io.Reader) (*http.Response, error) {
-	o := &client.Options{
-		Method: "POST",
-		URL:    url,
-		Body:   body,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Delete(url string, body io.Reader) (*http.Response, error) {
-	o := &client.Options{
-		Method: "DELETE",
-		URL:    url,
-		Body:   body,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Patch(url string, body io.Reader) (*http.Response, error) {
-	o := &client.Options{
-		Method: "PATCH",
-		URL:    url,
-		Body:   body,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Options(url string, body io.Reader) (*http.Response, error) {
-	o := &client.Options{
-		Method: "OPTIONS",
-		URL:    url,
-		Body:   body,
-	}
-	return r.Send(o)
-}
-
-func (r *Resilient) Custom() *client.Options {
-	return &client.Options{Method: "GET"}
-}
-
-func (r *Resilient) Send(o *client.Options) (*http.Response, error) {
-	return NewRequest(r, o)
-}
+// TODO: add consul client
